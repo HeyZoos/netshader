e20df214166274a6e81a93a5fe5db169d68405b6
Jesus Bracho <jessebracho@gmail.com>
2020-01-17 18:54:28 -0500

    Create an empty html app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+
+struct Model { }
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
